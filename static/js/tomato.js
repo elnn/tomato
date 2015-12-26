83ed9aa6e73f0fbdbd4312c917d7b981df903aa3
Daegeun Lee <elnn@elnn.kr>
2015-12-27 02:01:37 +0900

    Change chart type from spline to column

--- static/js/tomato.js
+++ static/js/tomato.js
@@ -2,6 +2,8 @@ $(function() {
   var ws;
   var valid = false;
   var series = null;
+  var bucket = null;
+  var t0;
 
   function connect() {
     var websocket = window['WebSocket'] || window['MozWebSocket'];
@@ -15,6 +17,15 @@ $(function() {
 
       ws.onopen = function() {
         console.log('WebSocket opened.');
+        series.setData([], false);
+        bucket = [];
+        t0 = (new Date()).getTime();
+        for (var i = 0; i < 30; ++i) {
+          var t = t0 + 1000 * 60 * i;
+          bucket.push({ size: 0, sum: 0.0 });
+          series.addPoint([t, 0.0], false);
+        }
+        series.chart.redraw();
       };
 
       ws.onclose = function() {
@@ -31,7 +42,17 @@ $(function() {
         }
         else if (json.type === 'score' && valid) {
           var now = (new Date()).getTime();
-          series[0].addPoint([now, json.value], true, false);
+          var idx = Math.floor((now - t0) / 1000 / 60);
+          if (0 <= idx && idx < 30) {
+            bucket[idx].size += 1;
+            bucket[idx].sum += Math.max(5.0, json.value);
+            var m = bucket[idx].sum / bucket[idx].size;
+            series.data[idx].update(m, true);
+          }
+          else {
+            // Force to close the connection.
+            $('#checkbox-connection').bootstrapSwitch('state', false);
+          }
         }
       };
     }
@@ -56,11 +77,11 @@ $(function() {
 
   $('#chart').highcharts({
     chart: {
-      type: 'spline',
+      type: 'column',
       animation: Highcharts.svg,
       events: {
         load: function() {
-          series = this.series;
+          series = this.series[0];
         }
       },
       style: {
@@ -70,18 +91,27 @@ $(function() {
     series: [{
       name: 'Score',
       data: [],
-      marker: { enabled: false },
     }],
-    title: { text: null },
-    legend: { enabled: true },
+    title: {
+      text: null,
+    },
+    tooltip: {
+      formatter: function() {
+        return Highcharts.numberFormat(this.y, 1);
+      },
+    },
+    legend: {
+      enabled: true,
+    },
     xAxis: {
-      visible: true,
       type: 'datetime',
     },
     yAxis: {
-      visible: true,
       min: 0.0,
       max: 100.0,
+      title: {
+        text: null,
+      },
     },
   });
 });
